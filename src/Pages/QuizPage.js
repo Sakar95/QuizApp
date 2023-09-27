3c571ef423a2501e599ccc1be7eec2200c21273a
Sakar <sk4sakar@gmail.com>
2023-09-27 18:58:53 +0530

    Timer Updated

--- src/Pages/QuizPage.js
+++ src/Pages/QuizPage.js
@@ -3,19 +3,62 @@ import Questions from '../Components/Questions';
 import { useQuizContext } from '../Context/useContext';
 import { Navigate, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faAngleLeft ,faSquarePollVertical} from '@fortawesome/free-solid-svg-icons';
+import { faAngleLeft, faSquarePollVertical } from '@fortawesome/free-solid-svg-icons';
 
 export default function QuizPage() {
-  const navigate = useNavigate()
-  const { quizQuestions,correctCount, setCorrectCount,  wrongCount, setWrongCount,currentQuestionIndex,setCurrentQuestionIndex } = useQuizContext();
-  
+  const navigate = useNavigate();
+  const {
+    quizQuestions,
+    correctCount,
+    setCorrectCount,
+    wrongCount,
+    setWrongCount,
+    currentQuestionIndex,
+    setCurrentQuestionIndex,
+  } = useQuizContext();
+
   const [timer, setTimer] = useState(10);
   const [shuffledOptions, setShuffledOptions] = useState([]);
   const [selectedOption, setSelectedOption] = useState(null);
-  
 
   // Calculate the total number of questions
   const totalQuestions = quizQuestions.length;
+
+  // Function to shuffle an array
+  const shuffleArray = (array) => {
+    const shuffled = [...array];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
+  };
+
+  // Function to handle option selection
+  const handleOptionSelect = (index) => {
+    // Check if an option has already been selected for this question
+    if (selectedOption === null) {
+      setSelectedOption(index);
+      // Check if the selected option is correct
+      if (shuffledOptions[index] === quizQuestions[currentQuestionIndex].correct_answer) {
+        setCorrectCount(correctCount + 1); // Increment correct count
+      }
+
+      // Delay the next question by 2 seconds
+      setTimeout(() => {
+        if (currentQuestionIndex < quizQuestions.length - 1) {
+          setCurrentQuestionIndex(currentQuestionIndex + 1);
+          setTimer(10); // Reset the timer for the next question
+          setSelectedOption(null); // Reset selected option
+          // If the timer runs out, consider the question unattempted
+          setWrongCount(wrongCount + 1);
+        } else {
+          // If there are no more questions, navigate to the result page
+          navigate("/result");
+        }
+      }, 1000); // 2 seconds delay
+    }
+  };
 
   useEffect(() => {
     // Shuffle options whenever the current question index changes
@@ -41,7 +84,7 @@ export default function QuizPage() {
           setWrongCount(wrongCount + 1);
         } else {
           clearInterval(interval); // Stop the timer when there are no more questions
-          navigate("/result")
+          navigate("/result");
         }
       }
     }, 1000); // Update the timer every second
@@ -51,36 +94,18 @@ export default function QuizPage() {
     };
   }, [currentQuestionIndex, quizQuestions, timer, wrongCount]);
 
-  // Function to shuffle an array
-  const shuffleArray = (array) => {
-    const shuffled = [...array];
-    for (let i = shuffled.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-    }
-    return shuffled;
-  };
-
-  // Function to handle option selection
-  const handleOptionSelect = (index) => {
-    // Check if an option has already been selected for this question
-    if (selectedOption === null) {
-      setSelectedOption(index);
-      // Check if the selected option is correct
-      if (shuffledOptions[index] === quizQuestions[currentQuestionIndex].correct_answer) {
-        setCorrectCount(correctCount + 1); // Increment correct count
-      }
-    }
-  };
-
   return (
     <>
-      <div className="w-screen  max-w-sm bg-yellow-400 h-60 rounded-3xl pt-8 h-1/3 p-4 relative">
-        <div className="flex justify-between ">
-          <div className="text-2xl text-bold text-white cursor-pointer" onClick={()=>navigate("/")}><FontAwesomeIcon icon={faAngleLeft} /></div>
-           <div className="cursor-pointer" onClick={()=>navigate("/result")}><FontAwesomeIcon icon={faSquarePollVertical} size='2xl' color='white' /></div>
+      <div className="w-screen max-w-sm bg-yellow-400 h-60 rounded-3xl pt-8 h-1/3 p-4 relative">
+        <div className="flex justify-between">
+          <div className="text-2xl text-bold text-white cursor-pointer" onClick={() => navigate("/")}>
+            <FontAwesomeIcon icon={faAngleLeft} />
+          </div>
+          <div className="cursor-pointer" onClick={() => navigate("/result")}>
+            <FontAwesomeIcon icon={faSquarePollVertical} size="2xl" color="white" />
+          </div>
         </div>
-        <div className="flex ">
+        <div className="flex">
           <div className="text-2xl text-bold text-white m-auto mt-8">{quizQuestions[0].category}</div>
         </div>
         {/* Timer */}
@@ -88,30 +113,30 @@ export default function QuizPage() {
           {timer}
         </div>
 
-       {/* Question div */}
-      <Questions/>
-        
+        {/* Question div */}
+        <Questions />
+
       </div>
-        {/* Shuffled Options */}
-        <div className="absolute bottom-12 w-64 left-1/2 transform -translate-x-1/2 p-2 shadow-md ">
-          {shuffledOptions.map((option, index) => (
-            <div
-              key={index}
-              className={`border cursor-pointer ${
-                selectedOption === index
-                  ? option === quizQuestions[currentQuestionIndex].correct_answer
-                    ? "border-green-500 border-2" // Correct option selected
-                    : "border-red-500 border-2" // Incorrect option selected
-                  : "border-orange-500"
-              } rounded px-2 mb-2`}
-              onClick={() => handleOptionSelect(index)}
-              // Disable the option if it's already selected
-              disabled={selectedOption !== null}
-            >
-              {option}
-            </div>
-          ))}
-        </div>
-        </>
+      {/* Shuffled Options */}
+      <div className="absolute bottom-12 w-64 left-1/2 transform -translate-x-1/2 p-2 shadow-md ">
+        {shuffledOptions.map((option, index) => (
+          <div
+            key={index}
+            className={`border cursor-pointer ${
+              selectedOption === index
+                ? option === quizQuestions[currentQuestionIndex].correct_answer
+                  ? "border-green-500 border-2" // Correct option selected
+                  : "border-red-500 border-2" // Incorrect option selected
+                : "border-orange-500"
+            } rounded px-2 mb-2`}
+            onClick={() => handleOptionSelect(index)}
+            // Disable the option if it's already selected
+            disabled={selectedOption !== null}
+          >
+            {option}
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
